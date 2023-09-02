dd09013bc70a1a02882c144b6705588534330b24
francollovera <francollovera@gmail.com>
2023-09-02 20:12:22 -0300

    Mensaje de commit descriptivo

--- script.js
+++ script.js
@@ -9,6 +9,25 @@ function mostrarOcultarMenu(){
         menuVisible = true;
     }
 }
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) =>{
+    const requestJson = await fetch(`./language/${language}.json`)
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+    
+        textToChange.innerHTML = texts[section][value];
+    }
+}
+flagsElement.addEventListener('click', (e)=>{
+    changeLanguage(e.target.parentElement.dataset.language);
+});
+
 
 function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
@@ -38,4 +57,4 @@ function efectoHabilidades(){
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
-} 
+}
